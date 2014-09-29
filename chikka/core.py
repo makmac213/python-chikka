6b1e967535b92508945da87d44618311ec322b27
josefmonje <josefmonje@gmail.com>
2014-09-29 13:41:45 +0800

    replying, exceptions
    improved functionality for replying, included exceptions in core

--- chikka/core.py
+++ chikka/core.py
@@ -2,10 +2,9 @@ import os
 import re
 import requests
 
-# chikka
-from .exceptions import (NullMobileNumberException, NullClientIDException,
-                            NullSecretKeyException, NullShortCodeException,
-                            InvalidMobileNumberException)
+# add local_settings.py to .gitignore
+# variables in local_settings so it won't be uploaded
+from local_settings import REQUEST_COST, CLIENT_ID, SECRET_KEY, SHORTCODE
 
 API_URL = 'https://post.chikka.com/smsapi/request'
 
@@ -20,7 +19,10 @@ class Chikka(object):
 
         # check and validate mobile number
         if not mobile_number:
+            print "Error: A Mobile number is required.\n"
             raise NullMobileNumberException
+        else:
+            mobile_number = str(mobile_number)
 
         # e.g. 09991234567
         if len(mobile_number) == 11 and mobile_number.startswith('0'):
@@ -28,37 +30,47 @@ class Chikka(object):
 
         # e.g. 639991234567
         if not re.match('^63[0-9]{10}', mobile_number):
+            print "Error: A valid Mobile number is required.\n"
             raise InvalidMobileNumberException
 
         payload['mobile_number'] = mobile_number
 
-        payload['message'] = message
-
-        payload['message_type'] = 'SEND'
+        # check if request_id was passed to this method
+        # means a message was received
+        # determines message_type, adds other required payload
+        if kwargs.get('request_id') is not None:
+            payload['request_id'] = kwargs.get('request_id')
+            payload['request_cost'] = REQUEST_COST
+            payload['message_type'] = 'REPLY'
+        else:
+            payload['message_type'] = 'SEND'
 
         # message_id can be passed to this method
         # this can be useful to track messages sent
         # however if message_id does not exist this method
         # will generate a random message id
-        message_id = kwargs.get('message_id', os.urandom(16).encode('hex'))
-        self.message_id = message_id
-        payload['message_id'] = message_id
+        payload['message_id'] = kwargs.get('message_id', os.urandom(16).encode('hex'))
+
+        payload['message'] = message
 
         self.response = requests.post(API_URL, data=payload)
 
 
     def _prepare_payload(self):
         # check if other required fields exists
-        client_id = getattr(self, 'client_id', None)
+        client_id = getattr(self, 'client_id', CLIENT_ID)
         if not client_id:
+            print "Error: Your Client ID is required.\n"
             raise NullClientIDException
 
-        secret_key = getattr(self, 'secret_key', None)
+        secret_key = getattr(self, 'secret_key', SECRET_KEY)
         if not secret_key:
+            print "Error: Your Secret Key is required.\n"
             raise NullSecretKeyException
 
-        shortcode = getattr(self, 'shortcode', None)
+        shortcode = getattr(self, 'shortcode', SHORTCODE)
         if not shortcode:
+            print "Error: Your shortcode is required.\n"
             raise NullShortCodeException
 
         payload = {
@@ -69,3 +81,19 @@ class Chikka(object):
 
         return payload
 
+# import of exceptions not working on mine
+# so placed it here. sorry.
+class NullMobileNumberException(Exception):
+    pass
+
+class InvalidMobileNumberException(Exception):
+    pass
+
+class NullClientIDException(Exception):
+    pass
+
+class NullSecretKeyException(Exception):
+    pass
+
+class NullShortCodeException(Exception):
+    pass
